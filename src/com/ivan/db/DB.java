811ab662e5d91190f8992e105f4e29f9c7b3569b
HelloWorld <87470666+IvanIsProgrammer@users.noreply.github.com>
2022-03-19 15:05:09 +0300

    commit

--- src/com/ivan/db/DB.java
+++ src/com/ivan/db/DB.java
@@ -24,6 +24,7 @@ public class DB {
                     props.getProperty("db.url"),
                     props.getProperty("db.user"),
                     props.getProperty("db.password")
+
             );
             statement = connection.createStatement();
         } catch (SQLException e) {
